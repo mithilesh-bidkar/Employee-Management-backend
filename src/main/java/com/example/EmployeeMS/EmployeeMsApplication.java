f1d7c71e810df389266e6440f8ceda3ad16ae67d
mithilesh bidkar <mithilesh.bidkar@gmail.com>
2023-11-05 13:32:40 +0530

    api completed

--- src/main/java/com/example/EmployeeMS/EmployeeMsApplication.java
+++ src/main/java/com/example/EmployeeMS/EmployeeMsApplication.java
@@ -1,18 +1,12 @@
 package com.example.EmployeeMS;
 
-import com.example.EmployeeMS.Config.MongoDBConfig;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
-@Import({MongoDBConfig.class})
 @EnableMongoAuditing
-@EnableAutoConfiguration
 public class EmployeeMsApplication {
 
 	public static void main(String[] args) {
